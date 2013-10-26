37c7e35a7a352f851bf68c57010a5656815898f6
Ollie Whitehouse <Ollie.Whitehouse@nccgroup.com>
2013-10-26 13:54:14 +0100

    Small batch file associated with copying the releases

---
+++ CopyReleases.bat
@@ -0,0 +1,6 @@
+@echo off
+copy .\ServicePerms\x64\Release\ServicePerms.exe .\Releases\ServicePerms64.exe
+copy .\FileSystemPerms\Release\FileSystemPerms.exe .\Releases\FileSystemPerms32.exe
+copy .\ProcessPerms\Release\ProcessPerms.exe .\Releases\ProcessPerms32.exe
+copy .\RegisteryPerms\x64\Release\RegisteryPerms.exe .\Releases\RegisteryPerms64.exe
+copy .\WinStationsAndDesktopPerms\Release\WinStationsAndDesktopPerms.exe .\Releases\WinStationsAndDesktopPerms32.exe
